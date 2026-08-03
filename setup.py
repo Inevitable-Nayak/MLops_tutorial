363f177923a552d84995a0ac92b0325902adbeca
Inevitable-Nayak <nayakamrutansu190@gmail.com>
2026-08-03 22:15:48 +0530

    add logging and the exception file

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup,find_packages
+hyphen_e_dot='-e .'
 def get_requirements(file_path:str)->list[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
     return requirements   
 setup(
     name='MLops',
